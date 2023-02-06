76b0db84dd1e51f8c047acf91c9c10ec198e7206
Leechiayu <gary86442@gmail.com>
2023-02-06 13:47:20 +0800

    feat: add template engine and static html

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const app = express();
 //設定連接埠:
 const port = 3000;
 
+const exphbs = require("express-handlebars");
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
 app.get("/", (req, res) => {
-  res.send("restaurant list");
+  res.render("index");
 });
 
 app.listen(port, () => {
